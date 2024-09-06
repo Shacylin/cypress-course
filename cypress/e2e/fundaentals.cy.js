e6922ebc2cdfa63028b2f51037b08ed37ab9cb72
Synthia Sau <sausynthia@gmail.com>
2024-09-06 10:30:16 +0300

    updated code

--- cypress/e2e/fundaentals.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Fundamentals test', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/fundamentals')
-    cy.get('[data-test="fundamentals-header]').contains(/Testing Fundamentals/i)
-
-  })
-})
